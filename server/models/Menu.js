9bdfc9c4fa97e2369ab7debdbc7e89487692a655
Fox <johnafoxwell@gmail.com>
2023-07-27 19:45:39 -0400

    Added menu model and seed for menu data

---
+++ server/models/Menu.js
@@ -0,0 +1,21 @@
+const { Schema, model } = require('mongoose');
+
+const menuSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+});
+
+const Menu = model('Menu', menuSchema);
+
+module.exports = Menu;
