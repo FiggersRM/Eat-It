8133dceeecd88c136d5151efe30f557fa7bbe007
amtune <allisontune@gmail.com>
2023-07-27 20:29:31 -0400

    materialize import

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import 'bootstrap/dist/css/bootstrap.min.css';
+import 'materialize-css/dist/css/materialize.min.css';
 import './index.css';
 import App from './App';
 
