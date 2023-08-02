2c51b9c358bdb922d29cc828a1c95ec081c23f1d
Fox <johnafoxwell@gmail.com>
2023-08-02 18:51:56 -0400

    Removed Manager from resaurant model and index.js

--- server/models/index.js
+++ server/models/index.js
@@ -2,6 +2,5 @@ const Restaurant = require("./Restaurant")
 const Menu = require("./Menu")
 const Address = require("./Address")
 const User = require("./User")
-const Manager = require("./Manager")
 
-module.exports = { Restaurant, Menu, Address, User, Manager };
+module.exports = { Restaurant, Menu, Address, User };
