d252c907e990de6d609de07ce0a77f3482f11388
amtune <allisontune@gmail.com>
2023-08-02 13:51:30 -0400

    set up layout for basic signup and layout from class activity

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,18 +1,18 @@
 import React from "react";
-import logo from "./images/eat-it-high-resolution-logo-color-on-transparent-background.png"
+import logo from "./images/eat-it-high-resolution-logo-color-on-transparent-background.png";
 
 function Footer() {
-    return (
-      <footer className="footer">
-        <div>
-            <img src={logo}/>
-        </div>
-        <p>123 Fake St. Richmond, VA 23221</p>
-        <b>
-        <p>© 2023 Eat It | All rights reserved.</p>
-        </b>
-      </footer>
-    )};
-
+	return (
+		<footer className="footer">
+			<div>
+				<img src={logo} />
+			</div>
+			<p>123 Fake St. Richmond, VA 23221</p>
+			<b>
+				<p>© 2023 Eat It | All rights reserved.</p>
+			</b>
+		</footer>
+	);
+}
 
 export default Footer;
