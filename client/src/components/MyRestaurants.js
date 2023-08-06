cbfd9ae876e826d8b410487648102f852c522c0b
FiggersRM <MitchFiggers@gmail.com>
2023-08-05 23:03:45 -0400

    Added the add restaurant form, still need code to insert into db.

--- client/src/components/MyRestaurants.js
+++ client/src/components/MyRestaurants.js
@@ -1,12 +1,74 @@
-import React from 'react';
+import React from "react";
+import { useState } from "react";
 
+import { QUERY_SINGLE_USER } from "../utils/queries";
+import { useParams } from "react-router-dom";
 
 function MyRestaurants() {
-  return (
-    <div>
+  const [showForm, setShowForm] = useState(false);
+  const [formState, setFormState] = useState({
+    name: "",
+    address: "",
+  });
 
-    </div>
-  )
+  const { userId } = useParams();
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    console.log(formState);
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  return (
+    <>
+      {showForm ? (
+        <div>
+          <form onSubmit={handleFormSubmit}>
+            <h1 className="dashH1">New Restaurant</h1>
+            <div className="restFormDiv">
+              <span className="inputLabel">Restaurant Name: </span>
+              <input
+                className="inputBox"
+                placeholder="Restaurant Name"
+                name="name"
+                type="name"
+                value={formState.name}
+                onChange={handleChange}
+              ></input>
+            </div>
+            <div className="restFormDiv">
+              <span className="inputLabel">Restaurant Address: </span>
+            <input
+              className="inputBox"
+              placeholder="Restaurant Address"
+              name="address"
+              type="address"
+              value={formState.address}
+              onChange={handleChange}
+            ></input>
+            </div>
+            <button className="formSubmitBtn" type="submit">
+              Create Restaurant
+            </button>
+          </form>
+        </div>
+      ) : (
+        <>
+          <button className="addRestBtn" onClick={() => setShowForm(!showForm)}>
+            Add a Restaurant
+          </button>
+        </>
+      )}
+    </>
+  );
 }
 
-export default MyRestaurants
+export default MyRestaurants;
